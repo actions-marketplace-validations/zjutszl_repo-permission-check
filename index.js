41249b3a74e3f79ca6f1aa20eaa9da4ad40274ba
zhili <zhilishen@yufuid.com>
2021-01-29 17:24:24 +0800

    Use vercel/ncc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  
+  core.setOutput('pass', true);
 } catch (error) {
   core.setFailed(error.message);
 }
