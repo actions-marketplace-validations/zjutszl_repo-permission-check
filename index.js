fd5e67b304b7cde12af188a2c96fb510e4383d06
zhili <zhilishen@yufuid.com>
2021-02-01 18:52:28 +0800

    chore: compile again!

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-import core from '@actions/core';
-import github from '@actions/github';
 import {Toolkit, ToolkitOptions} from 'actions-toolkit';
-
-const octokit = new github.Github(process.env.GITHUB_TOKEN)
 
 const findRepositoryInformation = (gitHubEventPath, log, exit) => {
   const payload = require(gitHubEventPath);
@@ -33,7 +29,7 @@ const fetchAllFiles = (listFiles, log, params, per_page, page) => {
 
 
 Toolkit.run(async function (toolKit) {
-  const patterns = core.getInput('files', { required: true });
+  const patterns = toolkit.inputs.files;
   toolkit.log.info(" files to check: ", patterns);
 
   if (!process.env.GITHUB_EVENT_PATH) {
@@ -59,15 +55,16 @@ Toolkit.run(async function (toolKit) {
         toolKit.log.debug("matched files: ", matchedFiles);
         if (matchedFiles.length > 0) return true;
 
-        core.setOutput('pass', true)
+        toolkit.outputs.pass = true
         toolkit.exit.success('Current Pull Request doesn\'t contain files match the rule.')
         return false; // TODO: 确认下是否要删除？
       })
       .then(async matched => {
         // 检查PR发起者的权限
         if (matched) { // TODO: 可能就不需要了
+            const octokit = new toolKit.github.Github(process.env.GITHUB_TOKEN)
             const perms = ["none", "read", "write", "admin"];
-            const actorName = github.context.actor;
+            const actorName = toolKit.github.context.actor;
             const response = await octokit.repos.getCollaboratorPermissionLevel({
               owner,
               repo,
@@ -75,19 +72,19 @@ Toolkit.run(async function (toolKit) {
             });
 
             let permission = response.data.permission; // Permission level of actual user
-            let argPerm = core.getInput("permission", { required: true }); // Permission level passed in through args
+            let argPerm = toolkit.inputs.permission; // Permission level passed in through args
 
             let yourPermIdx = perms.indexOf(permission);
             let requiredPermIdx = perms.indexOf(argPerm);
 
-            core.debug(`[Action] User Permission: ${permission}`);
-            core.debug(`[Action] Minimum Action Permission: ${argPerm}`);
+            toolkit.log.debug(`[Action] User Permission: ${permission}`);
+            toolkit.log.debug(`[Action] Minimum Action Permission: ${argPerm}`);
 
-            core.setOutput('pass', yourPermIdx >= requiredPermIdx)
+            toolkit.outputs.pass = yourPermIdx >= requiredPermIdx
         }
       })
       .catch(reason => {
-        core.setOutput('pass', false);
+        toolkit.outputs.pass = false
         toolkit.exit.failure(reason)
       })
     toolkit.exit.success('check finished!')
